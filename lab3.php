c26dbee9f1219f1c54cf383b2e6fc59f9a50c390
thanadet63070073 <73652820+thanadet63070073@users.noreply.github.com>
2020-11-15 20:44:37 +0700

    Update lab3.php

--- lab3.php
+++ lab3.php
@@ -23,7 +23,7 @@ $res = mysqli_query($conn, 'SELECT * FROM lab3');
     <tr>
       <th width="100"> <div align="center">Name</div></th>
       <th width="350"> <div align="center">Comment </div></th>
-      <th width="150"> <div align="center">Link </div></th>
+      <th width="150"> <div align="center">Action </div></th>
     </tr>
   </thead>
 <?php
@@ -33,7 +33,6 @@ while($Result = mysqli_fetch_array($res))
   <tr>
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
-    <td><?php echo $Result['Action'];?></td>
   </tr>
 <?php
 }
