0bb6a58be280e13942d1674638cc5aca4bf43539
thanadet63070073 <73652820+thanadet63070073@users.noreply.github.com>
2020-11-15 20:41:50 +0700

    Update lab3.php

--- lab3.php
+++ lab3.php
@@ -33,7 +33,7 @@ while($Result = mysqli_fetch_array($res))
   <tr>
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
-    <td><?php echo $Result['Link'];?></td>
+    <td><?php echo $Result['Action'];?></td>
   </tr>
 <?php
 }
