2ccf18f567e68b8458668115751d25dc353f8a88
thanadet63070073 <73652820+thanadet63070073@users.noreply.github.com>
2020-11-17 14:34:27 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -4,7 +4,7 @@ $conn = mysqli_connect('lab3itf.mysql.database.azure.com', 'tee@lab3itf', 'thana
 if (!$conn) {
 	die("Connection : failed (เชื่อมต่อฐานข้อมูล ไม่ สำเร็จ)" . mysqli_connect_error());
 } else {
-	echo "Connection : OK (เชื่อมต่อฐานข้อมูลสำเร็จ)";
+	"Connection : OK (เชื่อมต่อฐานข้อมูลสำเร็จ)";
 }
 
 mysqli_close($conn);
