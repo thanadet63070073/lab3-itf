691d0815418f8763407a9d761c3ddbbe494cb40b
thanadet63070073 <73652820+thanadet63070073@users.noreply.github.com>
2020-11-17 20:59:22 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,28 +1,30 @@
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-</head>
+<head></head>
 <body>
 <?php
+$delete_ID  = $_REQUEST['id'];
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'lab3itf.mysql.database.azure.com', 'tee@lab3itf', 'thanadet123@', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
+$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+$sql = '
+    DELETE FROM guestbook
+    WHERE id = ' . $delete_ID . ';
+    ';
 
-$strsql = "delete form guestbook where id = '".$_GET["id"]."' ";
-$objquery = mysql_quary($strsql);
-if($objquery)
-{
-    echo "Sucess"
-}
-else
-{
-    echo "Fail"
+$objQuery = mysqli_query($conn, $sql);
+if ($objQuery) {
+    echo "Record " . $delete_ID . " was Deleted.";
+} else {
+    echo "Error : Delete";
 }
 
 mysqli_close($conn); // ปิดฐานข้อมูล
+echo "<br><br>";
+echo "--- END ---";
 ?>
 </body>
 </html>
