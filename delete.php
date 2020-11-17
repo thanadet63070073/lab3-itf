2cee7563d36320ac73fd4eba78e512c1c57d7e5f
thanadet63070073 <73652820+thanadet63070073@users.noreply.github.com>
2020-11-17 17:01:20 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -2,13 +2,13 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 </head>
 <?php
-$delete_name  = $_REQUEST['Name_ID'];
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'lab3itf.mysql.database.azure.com', 'tee@lab3itf', 'thanadet123@', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
+$delete_name = $_REQUEST['Name_ID'];
 $sql = 'DELETE FROM guestbook WHERE Name_ID = ' . $delete_name . ';';
 
 $objQuery = mysqli_query($conn, $sql);
