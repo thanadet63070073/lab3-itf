6cbc601b39a23dbb01513dfdeb952e3db67f7f89
thanadet63070073 <73652820+thanadet63070073@users.noreply.github.com>
2020-11-17 17:32:41 +0700

    Add files via upload

--- delete.php
+++ delete.php
@@ -2,21 +2,24 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 </head>
 <?php
+$delete_ID  = $_REQUEST['Name_ID'];
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'lab3itf.mysql.database.azure.com', 'tee@lab3itf', 'thanadet123@', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-$strsql = "delete form guestbook where Name_ID = '".$_GET["Name_ID"]."' ";
-
-$objQuery = mysqli_query($strsql);
+$sql = '
+    DELETE FROM guestbook
+    WHERE Name_ID = ' . $delete_ID . ';
+    ';
+$objQuery = mysqli_query($conn, $sql);
 if ($objQuery) {
-    echo "Success ";
+    echo "Record " . $delete_ID . " was Deleted.";
 } else {
-    echo "Error ";
+    echo "Error : Delete";
 }
+
 mysqli_close($conn); // ปิดฐานข้อมูล
 echo "<br><br>";
-echo "--- END ---";
 ?>
