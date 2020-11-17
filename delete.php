80503de154e9e82b63ea7974c7c09f573fa8b33f
thanadet63070073 <73652820+thanadet63070073@users.noreply.github.com>
2020-11-17 18:26:00 +0700

    Add files via upload

--- delete.php
+++ delete.php
@@ -2,21 +2,23 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 </head>
 <?php
+$delete_ID  = $_REQUEST['Name_ID'];
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'lab3itf.mysql.database.azure.com', 'tee@lab3itf', 'thanadet123@', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-$name_id = $_GET['Name_ID']
-$q = "DELETE FROM guestbook WHERE Name_ID='$name_id'";
-$objQuery = mysqli_query($conn, $q);
+$sql = '
+    DELETE FROM guestbook
+    WHERE Name_ID = ' . $delete_ID . ';
+    ';
+$objQuery = mysqli_query($conn, $sql);
 if ($objQuery) {
-    header("Location: face.php");
+    echo "Record " . $delete_ID . " was Deleted.";
 } else {
-    echo "Error";
+    echo "Error : Delete";
 }
 
 mysqli_close($conn); // ปิดฐานข้อมูล
-echo "<br><br>";
 ?>
--- face.php
+++ face.php
@@ -15,7 +15,6 @@ $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
 <table border="1">
   <tr>
-    <th width="500"> <div align="center">ID</div></th>
     <th width="500"> <div align="center">Name</div></th>
     <th width="1000"> <div align="center">Comment </div></th>
     <th width="500"> <div align="center">Action </div></th>
@@ -25,15 +24,24 @@ while($Result = mysqli_fetch_array($res))
 {
 ?>
   <tr>
-    <td><?php echo $Result['Name_ID'];?></div></td>
+    <td><?php $Result['Name_ID'];?></div></td>
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
-    <td><a href="delete.php?id=<?php echo $objResult['Name_ID'];?>">ลบ</a></td>
+    <td>
+      <form name="Name_ID" action="delete.php" medthod="post">
+        <div align="center"><input type="submit" name="Name_ID" value="ลบ" /></div>
+      </form>
+    </td>
   </tr>
 <?php
 }
 ?>
 </table>
+</body>
+<body>
+  <form name="add_form" action="form.html" medthod="post"><br>
+    <div align="center"><input type="submit"  value="เพิ่ม" /></div>
+  </form>
 <?php
 mysqli_close($conn);
 ?>
