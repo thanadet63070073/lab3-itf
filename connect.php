9c04eec66258f45fcee8ce416201bf9d40c09afe
thanadet63070073 <73652820+thanadet63070073@users.noreply.github.com>
2020-11-17 14:55:34 +0700

    Delete connect.php

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'lab3itf.mysql.database.azure.com', 'tee@lab3itf', 'thanadet123@', 'itflab', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-mysqli_close($conn);
-?>
