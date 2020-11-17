933cdde497ab1dbdfabb1d978475ba9bf1f99015
thanadet63070073 <73652820+thanadet63070073@users.noreply.github.com>
2020-11-17 14:07:23 +0700

    Update lab3.php

--- lab3.php
+++ lab3.php
@@ -9,27 +9,9 @@ mysqli_real_connect($conn, 'lab3itf.mysql.database.azure.com', 'tee@lab3itf', 't
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
+} else {
+    echo "Connection : OK"
 }
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
-?>
-<table width="600" border="1">
-  <tr>
-    <th width="100"> <div align="center">Name</div></th>
-    <th width="350"> <div align="center">Comment </div></th>
-  </tr>
-<?php
-while($Result = mysqli_fetch_array($res))
-{
-?>
-  <tr>
-    <td><?php echo $Result['Name'];?></div></td>
-    <td><?php echo $Result['Comment'];?></td>
-  </tr>
-<?php
-}
-?>
-</table>
-<?php
 mysqli_close($conn);
 ?>
 </body>
