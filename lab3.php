07e58e75e6972fb2c0b6e0a96362fdde71c96d6c
thanadet63070073 <73652820+thanadet63070073@users.noreply.github.com>
2020-11-17 14:10:34 +0700

    Update lab3.php

--- lab3.php
+++ lab3.php
@@ -1,18 +1,10 @@
-<html>
-<head>
-<title>ITF Lab</title>
-</head>
-<body>
 <?php
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'lab3itf.mysql.database.azure.com', 'tee@lab3itf', 'thanadet123@', 'itflab', 3306);
-if (mysqli_connect_errno($conn))
-{
+if (!$conn){
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 } else {
     echo "Connection : OK"
 }
 mysqli_close($conn);
 ?>
-</body>
-</html>
