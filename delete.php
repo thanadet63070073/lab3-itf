dd4d16b35e866af3ea7b97c3072306ba8b413348
thanadet63070073 <73652820+thanadet63070073@users.noreply.github.com>
2020-11-17 17:49:37 +0700

    Add files via upload

--- delete.php
+++ delete.php
@@ -2,22 +2,19 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 </head>
 <?php
-$delete_ID  = $_REQUEST['Name_ID'];
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'lab3itf.mysql.database.azure.com', 'tee@lab3itf', 'thanadet123@', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-$sql = '
-    DELETE FROM guestbook
-    WHERE Name_ID = ' . $delete_ID . ';
-    ';
-$objQuery = mysqli_query($conn, $sql);
+$name_id = $_GET['name_id']
+$q = "DELETE FROM name_id WHERE name_id='$name_id'";
+$objQuery = mysqli_query($conn, $q);
 if ($objQuery) {
-    echo "Record " . $delete_ID . " was Deleted.";
+    header("Location: face.php");
 } else {
-    echo "Error : Delete";
+    echo "Error";
 }
 
 mysqli_close($conn); // ปิดฐานข้อมูล
--- face.php
+++ face.php
@@ -26,7 +26,7 @@ while($Result = mysqli_fetch_array($res))
   <tr>
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
-    <td align="center"><a href="delete.php?Name_ID=<?php echo $objResult["Name_ID"]; ?>">Delete</a></td>
+    <td align="center"><a href="delete.php?name_id=<?php echo $objResult['name_id']; ?>">ลบ</a></td>
   </tr>
 <?php
 }
