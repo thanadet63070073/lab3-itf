04906ad470456eb43148daded2240791050a31a4
thanadet63070073 <73652820+thanadet63070073@users.noreply.github.com>
2020-11-17 14:21:00 +0700

    Update lab3.php

--- lab3.php
+++ lab3.php
@@ -1,37 +1,11 @@
-<html>
-<head>
-<title>ITF LAB</title>
-</head>
-<body>
 <?php
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'lab3itf.mysql.database.azure.com', 'tee@lab3itf', 'thanadet123@', 'itflab', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
+$conn = mysqli_connect('lab3itf.mysql.database.azure.com', 'tee@lab3itf', 'thanadet123@', 'itflab', 3306);
+
+if (!$conn) {
+	die("Connection : failed (เชื่อมต่อฐานข้อมูล ไม่ สำเร็จ)" . mysqli_connect_error());
+} else {
+	echo "Connection : OK (เชื่อมต่อฐานข้อมูลสำเร็จ)";
 }
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
-?>
-<table width="600" border="1">
-  <tr>
-    <th width="100"> <div align="center">Name</div></th>
-    <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Action </div></th>
-  </tr>
-<?php
-while($Result = mysqli_fetch_array($res))
-{
-?>
-  <tr>
-    <td><?php echo $Result['Name'];?></div></td>
-    <td><?php echo $Result['Comment'];?></td>
-  </tr>
-<?php
-}
-?>
-</table>
-<?php
+
 mysqli_close($conn);
 ?>
-</body>
-</html>
