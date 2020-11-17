28a36557e821121647a75ff3cbbae1fc2e61f51a
thanadet63070073 <73652820+thanadet63070073@users.noreply.github.com>
2020-11-17 14:01:39 +0700

    Update lab3.php

--- lab3.php
+++ lab3.php
@@ -1,12 +1,6 @@
 <html>
 <head>
-  <title>ITF Lab</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<title>ITF Lab</title>
 </head>
 <body>
 <?php
@@ -16,16 +10,13 @@ if (mysqli_connect_errno($conn))
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-$res = mysqli_query($conn, 'SELECT * FROM lab3');
+$res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<table width="600" border="1" class="table table-dark table-striped">>
-  <thead>
-    <tr>
-      <th width="100"> <div align="center">Name</div></th>
-      <th width="350"> <div align="center">Comment </div></th>
-      <th width="150"> <div align="center">Action </div></th>
-    </tr>
-  </thead>
+<table width="600" border="1">
+  <tr>
+    <th width="100"> <div align="center">Name</div></th>
+    <th width="350"> <div align="center">Comment </div></th>
+  </tr>
 <?php
 while($Result = mysqli_fetch_array($res))
 {
