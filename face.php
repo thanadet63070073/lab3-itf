c40cc72882066a9c3ee19ff164df1610f594c3b2
thanadet63070073 <73652820+thanadet63070073@users.noreply.github.com>
2020-11-17 22:03:21 +0700

    Update face.php

--- face.php
+++ face.php
@@ -29,7 +29,7 @@ while($Result = mysqli_fetch_array($res))
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
     <td><a href="delete.php?id=<?php echo result["id"];?>">ลบ</a>
-  <a href="edit.php?id=<?php echo $result["id"];?>แก้ไข</td>
+  <a href="edit.php?id=<?php echo $result["id"];?>">แก้ไข</td>
   </tr>
 <?php
 }
