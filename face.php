fa9ce67aa570237c1f356ed99bbca48b2fb26354
thanadet63070073 <73652820+thanadet63070073@users.noreply.github.com>
2020-11-17 20:56:08 +0700

    Update face.php

--- face.php
+++ face.php
@@ -26,7 +26,7 @@ while($Result = mysqli_fetch_array($res))
   <tr>
     <td><?php echo $Result['Name'];?></div></td>
     <td><?php echo $Result['Comment'];?></td>
-    <td><a href="delete.php?id=<?php echo $objresult["id"];?>">ลบ</a></td>
+    <td><a href="https://lab3-itf.azurewebsites.net/delete.php?id=<?php echo $objresult["id"];?>">ลบ</a></td>
   </tr>
 <?php
 }
