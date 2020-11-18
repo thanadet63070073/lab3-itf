9a178f3ba6cf338541a31625b84dd42d74748d6b
thanadet63070073 <73652820+thanadet63070073@users.noreply.github.com>
2020-11-18 22:36:18 +0700

    Add files via upload

--- insert.php
+++ insert.php
@@ -6,18 +6,17 @@ $conn = mysqli_init();
 mysqli_real_connect($conn, 'lab3itf.mysql.database.azure.com', 'tee@lab3itf', 'thanadet123@', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
 {
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
+  die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 
 $name = $_POST['name'];
 $comment = $_POST['comment'];
 $sql = "INSERT INTO guestbook (Name , Comment) VALUES ('$name', '$comment')";
 
-if (mysqli_query($conn, $sql)) {
-  header("Location: face.php");
-  } else {
+if (mysqli_query($conn, $sql)){
+    header("Location: face.php");
+} else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-  }
-  
+}
 mysqli_close($conn);
 ?>
