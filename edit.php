a2854a0ec12c7eacc4a6ad55e74edf16f19788cb
thanadet63070073 <73652820+thanadet63070073@users.noreply.github.com>
2020-11-18 21:14:18 +0700

    Add files via upload

--- edit.php
+++ edit.php
@@ -1,4 +1,5 @@
 <?php
+
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'lab3itf.mysql.database.azure.com', 'tee@lab3itf', 'thanadet123@', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
@@ -9,6 +10,7 @@ if (mysqli_connect_errno($conn))
 $id = $_GET['id'];
 $name = $_POST['name'];
 $comment = $_POST['comment'];
+
 $sql = "UPDATE guestbook SET Name = '$name', Comment = '$comment' WHERE id = '$id'"; 
 if (mysqli_query($conn, $sql)) {
     echo "Update sucess"; 
